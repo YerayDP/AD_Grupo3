adf460d25b12c75541f3753fb843c0e025260a8b
pablodelgadom <pablo00dm00@gmail.com>
2021-11-23 19:16:40 +0100

    Models

--- AD_Grupo3/src/main/java/com/example/demo/entity/RRHH.java
+++ AD_Grupo3/src/main/java/com/example/demo/entity/RRHH.java
@@ -1,5 +1,97 @@
 package com.example.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class RRHH {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	private String nombre;
+	
+	private String apellidos;
+	
+	private String email;
+	
+	private String password;
+	
+	private String String;
+	
+	private String empresa;
+	
+	@OneToMany(mappedBy="rrhh", orphanRemoval=true)
+	private List<Ciclo> ofertas = new ArrayList<>();
+
+	public RRHH(int id, String nombre, String apellidos, String email,
+			String password, String string, String empresa) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.password = password;
+		String = string;
+		this.empresa = empresa;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	
+		
+	
 
 }
