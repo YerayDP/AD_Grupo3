0d9d837020efb26344a58a49e1f543c63c75da4b
pablodelgadom <pablo00dm00@gmail.com>
2021-12-02 19:56:55 +0100

    Registro

--- AD_Grupo3/src/main/java/com/example/demo/controller/LoginController.java
+++ AD_Grupo3/src/main/java/com/example/demo/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.example.demo.entity.User;
 import com.example.demo.service.impl.UserService;
 
+@Controller
 public class LoginController {
 
 	@Autowired
