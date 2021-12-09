a20b81b0a41c42c529adb8ea7c32dce8f38e006d
YerayDP <73529878+YerayDP@users.noreply.github.com>
2021-12-09 21:07:10 +0100

    Profile

--- AD_Grupo3/src/main/java/com/example/demo/controller/UserController.java
+++ AD_Grupo3/src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,32 +27,61 @@ import com.example.demo.service.CicloService;
 @RequestMapping("/user")
 public class UserController {
 	private static  String USER_VIEW="User";
+	private static  String INDEX_VIEW="index";
 	@Autowired
 	@Qualifier("userService")
 	private UserService userService;
 	
 	@Autowired
 	@Qualifier("cicloService")
-	private CicloService cicloService;
+	private CicloService cicloService; 
 	
 	@Autowired
 	@Qualifier("userRepository")
 	private UserRepository userRepository;
 	
-	@GetMapping("/update/{id}")
-	public String formUser(@PathVariable (name="id", required=false) Integer id, Model model) {
-		
-		
-		model.addAttribute("ciclos", cicloService.listAllCiclos());
-		if(id==null) 
-			model.addAttribute("user", new UserModel());
-		else
-			model.addAttribute("user", userService.findUserId(id));
-		return USER_VIEW;
+	@GetMapping("/")
+	public String Volver() {
+	    return "index";
 	}
+	
+	@GetMapping("/indexRRHH")
+	public String showRRHH(Model model) {
+		String id= SecurityContextHolder.getContext().getAuthentication().getName();
+	    model.addAttribute("users", userRepository.findByEmail(id));
+	    return "profile";
+	}
+	
+	@GetMapping("/indexAlumnos")
+	public String showUser(Model model) {
+		String id= SecurityContextHolder.getContext().getAuthentication().getName();
+	    model.addAttribute("users", userRepository.findByEmail(id));
+	    return "profile";
+	}
+	
+	@GetMapping("/index")
+	public String show(Model model) {
+		String id= SecurityContextHolder.getContext().getAuthentication().getName();
+	    model.addAttribute("users", userRepository.findByEmail(id));
+	    return "profile";
+	}
+	
+	@PostMapping("/update/{id}")
+	public String addStudent(@ModelAttribute("user")User userModel,Model model)
+	{
+		if(userModel.getId()==0)
+			userService.registrar(userModel);
+		else
+			userRepository.save(userModel);
+		
+		return "redirect:/user/index";
+	}
+	
 
+	
 	@GetMapping("/edit/{id}")
 	public String showUpdateForm(@PathVariable("id") long id, Model model) {
+		model.addAttribute("ciclos", cicloService.listAllCiclos());
 	    User user = userRepository.findById(id);
 	    model.addAttribute("user", user);
 	    return "User";
