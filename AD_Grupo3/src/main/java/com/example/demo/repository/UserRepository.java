4502ec55c10d7f012875e1977138d74ccfaedb81
pablodelgadom <pablo00dm00@gmail.com>
2021-12-13 11:52:15 +0100

    Registro arreglado

--- AD_Grupo3/src/main/java/com/example/demo/repository/UserRepository.java
+++ AD_Grupo3/src/main/java/com/example/demo/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.User;
-import com.example.demo.models.UserModel;
 
 @Repository("userRepository")
 public interface UserRepository extends JpaRepository <User, Serializable>{
--- AD_Grupo3/src/main/java/com/example/demo/seguridad/SecurityConfig.java
+++ AD_Grupo3/src/main/java/com/example/demo/seguridad/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/*","/auth/login","assets/**","/css/**","/js/**","/public/**","/static/**", "/auth/registerForm").permitAll()
+		.antMatchers("/","/auth/**","assets/**","/css/**","/js/**","/public/**","/static/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
