cd9a56a7a879c14f3c618ebad92cf43a631df07b
pablodelgadom <pablo00dm00@gmail.com>
2022-01-09 16:43:56 +0100

    EEE

---
+++ AD_Grupo3/src/main/java/com/example/demo/controller/OfertaController.java
@@ -0,0 +1,68 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.entity.Oferta;
+import com.example.demo.models.OfertaModel;
+import com.example.demo.service.OfertaService;
+
+@Controller
+@RequestMapping("/ofertas")
+public class OfertaController {
+
+		@Autowired
+		private OfertaService ofertaService;
+		
+		private static  String OFERTAS_VIEW="ofertas";
+		private static  String FORM="Form_ofertas";
+		
+		@GetMapping("/listOfertas")
+		public ModelAndView listOfertas()
+		{
+			ModelAndView mav = new ModelAndView(OFERTAS_VIEW);
+			mav.addObject("ofertas", ofertaService.listAllOfertas());
+			return mav; 
+		}
+		
+		@PreAuthorize("hasRole('ROLE_ADMIN')")
+		@PostMapping("/addOferta")
+		public String addOferta(@ModelAttribute("oferta")OfertaModel OfertaModel,Model model)
+		{
+			if(OfertaModel.getId()==0)
+				ofertaService.addOferta(OfertaModel);
+			else
+				ofertaService.updateOferta(OfertaModel);
+			
+			return "redirect:/ofertas/listOfertas";
+		}
+		
+		@PreAuthorize("hasRole('ROLE_ADMIN')")
+		@GetMapping(value={"/formOferta/", "/formOferta/{id}"})
+		public String formOferta(@PathVariable(name="id", required=false)Integer id,Model model)
+		{
+			if(id==null)
+				model.addAttribute("oferta",new Oferta());
+			else
+				model.addAttribute("oferta",ofertaService.findOferta(id));
+			return FORM;
+		}
+		
+		@PreAuthorize("hasRole('ROLE_ADMIN')")
+		@GetMapping("/delete/{id}")
+		public String deleteOferta(@PathVariable("id")int id)
+		{
+			ofertaService.removeOferta(id);
+			return "redirect:/ofertas/listOfertas";
+		}
+	
+}
