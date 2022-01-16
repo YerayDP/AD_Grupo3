32686d1af1a457b7b9fcb88588581cc571fc259d
pablodelgadom <pablo00dm00@gmail.com>
2022-01-16 20:18:53 +0100

    Arreglos

--- AD_Grupo3/src/main/java/com/example/demo/service/InscritoService.java
+++ AD_Grupo3/src/main/java/com/example/demo/service/InscritoService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 
 import com.example.demo.entity.Inscrito;
+import com.example.demo.entity.User;
 import com.example.demo.models.InscritoModel;
 
 public interface InscritoService {
@@ -12,6 +13,8 @@ public interface InscritoService {
 	int removeInscrito(int id);
 	Inscrito updateInscrito(InscritoModel InscritoModel);
 	InscritoModel findInscrito(int id);
+	List <InscritoModel> findByUsuario(User user);
+	Inscrito transform(InscritoModel Inscritomodel);
 	//abstract List<InscritoModel> listAllInscritos(InscritoModel InscritoModel);
 	
 }
--- AD_Grupo3/src/main/java/com/example/demo/service/OfertaService.java
+++ AD_Grupo3/src/main/java/com/example/demo/service/OfertaService.java
@@ -3,10 +3,10 @@ package com.example.demo.service;
 import java.util.Date;
 import java.util.List;
 
+import com.example.demo.entity.Ciclo;
 import com.example.demo.entity.Oferta;
 import com.example.demo.entity.User;
 import com.example.demo.models.OfertaModel;
-import com.example.demo.models.UserModel;
 
 public interface OfertaService {
 
@@ -16,7 +16,9 @@ public interface OfertaService {
 	Oferta updateOferta(OfertaModel OfertaModel);
 	OfertaModel findOferta(int id);
 	List<OfertaModel> findByUsuario(User user);
+	List<OfertaModel> findByCiclo(Ciclo ciclo);
 	List<OfertaModel> findByFechamaxBefore(Date fecha);
+	Oferta transform(OfertaModel Ofertamodel);
 	//abstract List<OfertaModel> listAllOfertas(OfertaModel OfertaModel);
 	
 }
