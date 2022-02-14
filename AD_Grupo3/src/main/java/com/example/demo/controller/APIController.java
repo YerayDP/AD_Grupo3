20fffe05a707b35e21fd3b33be8836dc1d8e7d03
pablodelgadom <pablo00dm00@gmail.com>
2022-02-14 17:42:27 +0100

    Back-End fixed

--- AD_Grupo3/src/main/java/com/example/demo/controller/APIController.java
+++ AD_Grupo3/src/main/java/com/example/demo/controller/APIController.java
@@ -17,13 +17,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
 import com.example.demo.models.InscritoModel;
+import com.example.demo.models.UserModel;
 import com.example.demo.service.InscritoService;
+import com.example.demo.service.impl.UserService;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -43,22 +46,31 @@ public class APIController {
 		@Qualifier("inscritoService")
 		private InscritoService inscritoService;
 		
+		@Autowired
+		@Qualifier("userService")
+		private UserService userService;
+		
 		
 		@GetMapping("/listInscritos")
 		public List<InscritoModel> listarInscrito(){
 			return inscritoService.listAllInscritos();
 		}
 		
-		@PostMapping("/login/")
-		public User login(@RequestParam("user") String username,@RequestParam("password") String password) {
-			Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
+		@PostMapping("/login")
+		public User login(@RequestBody User user) {
+			System.out.println(user.getEmail());
+			System.out.println(user.getPassword());
+			Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword()));
 			SecurityContextHolder.getContext().setAuthentication(authentication);
-			String token = getJWTToken(username);
-			User user = new User();
-			user.setEmail(username);
-			user.setPassword(password);
-			user.setToken(token);
-			return user;
+			String token = getJWTToken(user.getEmail());
+			User u = new User();
+			u = userService.transform(userService.findStudentMail(user.getEmail()));
+			u.setToken(token);
+			System.out.println("Token "+u.getToken());
+			System.out.println(u.getRole());
+			System.out.println(u.getEmail());
+			System.out.println(u.getPassword());
+			return u;
 		}
 		private String getJWTToken(String username) {
 			String secretKey = "mySecretKey";
